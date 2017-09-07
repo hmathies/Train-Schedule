f97f1f5888e0f3628770df4effe4ab9cb5b56717
Heather Mathies <heathermathies@Heathers-MacBook-Air.local>
2017-09-06 20:39:31 -0400

    changed to add a table instead of divs and changed for loop

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,68 @@
  /*Heather Mathies built September 2017*/
- /*----when the page loads*/
-  $(document).ready(function() {
 
-//initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDayqtJ-tTarim-e55T56edQWDn-omh0gs",
-    authDomain: "train-app-7b721.firebaseapp.com",
-    databaseURL: "https://train-app-7b721.firebaseio.com",
-    projectId: "train-app-7b721",
-    storageBucket: "",
-    messagingSenderId: "45457176829"
-  };
-  firebase.initializeApp(config);
-  //variable to reference the database
-  var database = firebase.database();
-  console.log("database says: " + database);
+ function firebaseConnection(){
+   var config = {
+         apiKey: "AIzaSyDayqtJ-tTarim-e55T56edQWDn-omh0gs",
+         authDomain: "train-app-7b721.firebaseapp.com",
+         databaseURL: "https://train-app-7b721.firebaseio.com",
+         projectId: "train-app-7b721",
+         storageBucket: "",
+         messagingSenderId: "45457176829"
+    };
+    firebase.initializeApp(config);
+         //variable to reference the database
+    var database = firebase.database();
+    /*-----------variable to reference a specific location in the database/firebase. All of the train data will be stored here. 
+    not sure if i need to add a separate folder because its always going to be train data and nothing else-------------------*/
+    //var trainref = database.ref();
+    /*-------this pushes the train data to firebase- but I'm not sure it's working---*/
+    //trainref.push(trains);
+ }
+
+ function initFormListener(){
+    $('#submit-train').on('click', function() {
+       event.preventDefault();
+       name = $('#train-name').val().trim();
+       destination = $('#destination').val().trim();
+       firstTime = $('#first-time').val().trim();
+       frequency = $('#frequency').val().trim();
+       var train = {
+          name : name,
+          destination: destination,
+          firstTime: firstTime,
+          frequency: frequency
+        }
+       trains.push(train);
+       listTrains();
+   });
+ }
+
+ function calculateNextArrival(firstTime,frequency){
+
+ }
+
+ function listTrains() {
+    var trainListTable = $('.train-list');
+       for(var i =0; i<trains.length; i++){
+          var train = trains[i];
+          nextArrival = calculateNextArrival(train.firstTime,train.frequency);
+          minutesAway = '';
+          var row = '<tr><td>'+train.name+'</td><td>'+train.destination+'</td><td>'+train.frequency+'</td><td>'+nextArrival+'</td><td>'+minutesAway+'</td></tr>';
+          trainListTable.append(row);
+       }
+  }
+
+var trains = [];
 
 
+ $(document).ready(function() {
+    firebaseConnection();
+    initFormListener();
+    listTrains();
+    sandbox();
 
-
-});
+    function sandbox(){
+      var res =moment('10:22').fromNow();
+      console.log(res);
+    }
+ });
